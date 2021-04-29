684d6c6599adf2f7bbab20eeef00c6d445317d3a
leealj <leeaxj@gmail.com>
2021-04-29 15:46:54 -0400

    added option for correlation distance

--- R/main_functions.R
+++ R/main_functions.R
@@ -3,7 +3,7 @@
 
 # Hierarchical clustering of high granularity clustering results
 # 
-# Development version 4:
+# March 2021:
 # - Improve speed of scan_dendrogram() by assigning cells to cluster # instead of
 #   iterating through each step (which results in re-calculating the clusters several times).
 # - Enabled subsetting with cell ids which will also subset the appropriate branches of 
@@ -11,7 +11,12 @@
 # - Implemented a streamlined multi-gene/multi-gene set analysis with sig_cells() and 
 #   scan_dendrogram(). Also updated feature_phylo() to provide a list of plots
 #   to accomodate for the multi-gene set analyses that are produced. 
-
+#
+# April 2021:
+# - Added parameter in recluster_all() to choose between euclidean or correlation 
+#   distance when performing BuildClusterTree.
+# - Modified BuildClusterTree from Seurat to be able to use correlation distance
+#   to calculate distance between centroids.
 ################################################################
 ################################################################
 
@@ -21,7 +26,8 @@
 # 3. for each step (increments of k + 1), add cluster assignments to meta data. 
 # 4. returns Seurat object with phylo object in slot = "BuildClusterTree" and 
 #    with all clustering assignments stored in meta data. 
-recluster_all <- function(object, ident = "seurat_clusters", dims = NULL) {
+recluster_all <- function(object, ident = "seurat_clusters", 
+                          dims = NULL, dist.method = "euclidean") {
   time.start <- Sys.time()
   object.phylo <- Tool(object, slot = "BuildClusterTree")
   
@@ -35,7 +41,7 @@ recluster_all <- function(object, ident = "seurat_clusters", dims = NULL) {
   
   if (length(VariableFeatures(object)) == 0) object <- FindVariableFeatures(object)
   if (is.null(object.phylo)) {
-    object <- BuildClusterTree(object, dims = dims, features = VariableFeatures(object), )
+    object <- BuildClusterTree(object, dims = dims, features = VariableFeatures(object))
     object.phylo <- Tool(object, slot = "BuildClusterTree")
   }
   
@@ -72,6 +78,148 @@ recluster_all <- function(object, ident = "seurat_clusters", dims = NULL) {
 
   time.end <- Sys.time()
   print(time.end-time.start)
+  return(object)
+}
+
+# Modified BuildClusterTree function from Seurat
+# Added "dist.method" parameter to indicate euclidean or correlation distance
+# to calculate distance between centroids.
+BuildClusterTree <- function(
+  object,
+  assay = NULL,
+  features = NULL,
+  dims = NULL,
+  graph = NULL,
+  slot = 'data',
+  reorder = FALSE,
+  reorder.numeric = FALSE,
+  verbose = TRUE,
+  dist.method = "euclidean"
+) {
+  require(stats)
+  require(utils)
+  PackageCheck <- function(..., error = TRUE) {
+    pkgs <- unlist(x = c(...), use.names = FALSE)
+    package.installed <- vapply(
+      X = pkgs,
+      FUN = requireNamespace,
+      FUN.VALUE = logical(length = 1L),
+      quietly = TRUE
+    )
+    if (error && any(!package.installed)) {
+      stop(
+        "Cannot find the following packages: ",
+        paste(pkgs[!package.installed], collapse = ', '),
+        ". Please install"
+      )
+    }
+    invisible(x = package.installed)
+  }
+  if (!PackageCheck('ape', error = FALSE)) {
+    stop(cluster.ape, call. = FALSE)
+  }
+  assay <- assay %||% DefaultAssay(object = object)
+  if (!is.null(x = graph)) { 
+    idents <- levels(x = object)
+    nclusters <- length(x = idents)
+    data.dist <- matrix(
+      data = numeric(length = 1L),
+      nrow = nclusters,
+      ncol = nclusters,
+      dimnames = list(idents, idents)
+    )
+    graph <- object[[graph]]
+    cxi <- CellsByIdentities(object = object)
+    cpairs <- na.omit(object = unique(x = t(x = apply(
+      X = expand.grid(1:nclusters, 1:nclusters)[, c(2, 1)],
+      MARGIN = 1,
+      FUN = function(x) {
+        if (length(x = x) == length(x = unique(x = x))) {
+          return(sort(x = x))
+        }
+        return(c(NA, NA))
+      }
+    ))))
+    if (verbose) {
+      pb <- txtProgressBar(style = 3, file = stderr())
+    }
+    for (i in 1:nrow(x = cpairs)) {
+      i1 <- cpairs[i, ][1]
+      i2 <- cpairs[i, ][2]
+      graph.sub <- graph[cxi[[idents[i1]]], cxi[[idents[i2]]]]
+      d <- mean(x = graph.sub)
+      if (is.na(x = d)) {
+        d <- 0
+      }
+      data.dist[i1, i2] <- d
+      if (verbose) {
+        setTxtProgressBar(pb = pb, value = i / nrow(x = cpairs))
+      }
+    }
+    if (verbose) {
+      close(con = pb)
+    }
+    diag(x = data.dist) <- 1
+    data.dist <- dist(x = data.dist)
+  } else if (!is.null(x = dims)) { 
+    my.lapply <- ifelse(test = verbose, yes = pbapply::pblapply, no = lapply)
+    embeddings <- Embeddings(object = object, reduction = 'pca')[, dims]
+    data.dims <- my.lapply(
+      X = levels(x = object),
+      FUN = function(x) {
+        cells <- WhichCells(object = object, idents = x)
+        if (length(x = cells) == 1) {
+          cells <- c(cells, cells)
+        }
+        temp <- colMeans(x = embeddings[cells, ])
+      }
+    )
+    data.dims <- do.call(what = 'cbind', args = data.dims)
+    colnames(x = data.dims) <- levels(x = object)
+    data.dist <- dist(x = t(x = data.dims))
+  } else {
+    features <- features %||% VariableFeatures(object = object)
+    features <- intersect(x = features, y = rownames(x = object))
+    data.avg <- AverageExpression( # calculating centroids
+      object = object,
+      assays = assay,
+      features = features,
+      slot = slot,
+      verbose = verbose
+    )[[1]]
+    if (dist.method == "euclidean") {
+      data.dist <- dist(x = t(x = data.avg[features, ])) 
+    } else if (dist.method == "correlation") {
+      data.dist <- as.dist(CorDist(t(data.avg), t(data.avg), method = "pearson"))
+    }
+  }
+  data.tree <- ape::as.phylo(x = hclust(d = data.dist))
+  Tool(object = object) <- data.tree
+  if (reorder) {
+    if (verbose) {
+      message("Reordering identity classes and rebuilding tree")
+    }
+    old.ident.order <- levels(x = object)
+    data.tree <- Tool(object = object, slot = 'BuildClusterTree')
+    all.desc <- GetDescendants(tree = data.tree, node = (data.tree$Nnode + 2))
+    all.desc <- old.ident.order[all.desc[all.desc <= (data.tree$Nnode + 1)]]
+    Idents(object = object) <- factor(x = Idents(object = object), levels = all.desc, ordered = TRUE)
+    if (reorder.numeric) {
+      new.levels <- sort(x = unique(x = as.integer(x = Idents(object = object))))
+      Idents(object = object) <- factor(x = as.integer(x = Idents(object = object)), levels = new.levels)
+      object[['tree.ident']] <- as.integer(x = Idents(object = object))
+    }
+    object <- BuildClusterTree(
+      object = object,
+      assay = assay,
+      features = features,
+      dims = dims,
+      graph = graph,
+      slot = slot,
+      reorder = FALSE,
+      verbose = verbose
+    )
+  }
   return(object)
 }
 
@@ -336,7 +484,7 @@ cluster_bool_mat <- function(object) {
 # cluster.labels must be a character vector in the same order as clusters to label 
 # from parameters node or gene_set, and must therefore be the same length.
 # Returns a Seurat object
-SetSigIdent <- function(object, sig_cells_output, gene_set = NULL, 
+SetSigIdent <- function(object, sig_cells_output = NULL, gene_set = NULL, 
                         nodes = NULL, label = "other", cluster.labels = NULL) {
   if (!is.null(gene_set)) sig_clusters <- sig_cells_output$clusters[[gene_set]]
   if (!is.null(nodes)) {
